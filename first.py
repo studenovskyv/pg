8cfc721b18ff6493399da0a41a2cea18480941ae
studenovskyv <vasikstudenovsky@seznam.cz>
2024-10-03 18:02:18 +0200

    first.py

--- first.py
+++ first.py
@@ -1,14 +1,13 @@
-# Definice funkce sudy_nebo_lichy
+
 def sudy_nebo_lichy(cislo):
-    # Kontrola, zda je číslo sudé nebo liché
+
     if cislo % 2 == 0:
         print(f"Číslo {cislo} je sudé")
     else:
         print(f"Číslo {cislo} je liché")
 
-# Získání čísla od uživatele
 try:
-    cislo = int(input("Zadejte číslo: "))  # Převod vstupu na celé číslo
+    cislo = int(input("Zadejte číslo: "))  
     sudy_nebo_lichy(cislo)
 except ValueError:
-    print("Zadejte prosím platné celé číslo.")  # Ošetření neplatného vstupu
+    print("Zadejte prosím platné celé číslo.")  
