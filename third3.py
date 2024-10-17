dd1364e4e629e0af5335e8cf8e197e0625cded41
studenovskyv <vasikstudenovsky@seznam.cz>
2024-10-17 10:59:55 +0200

    third.py

--- third3.py
+++ third3.py
@@ -1,9 +1,5 @@
 def je_prvocislo(cislo):
-    """
-    Funkce ověří, zda zadané číslo je nebo není prvočíslo a vrátí True nebo False.
-
-    Prvočíslo je takové číslo větší než 1, které není dělitelné žádným jiným číslem kromě 1 a sebe sama.
-    """
+    
     if cislo < 2:
         return False
     for i in range(2, int(cislo ** 0.5) + 1):
@@ -12,9 +8,7 @@ def je_prvocislo(cislo):
     return True
 
 def vrat_prvocisla(maximum):
-    """
-    Funkce spočítá všechna prvočísla v rozsahu 1 až maximum a vrátí je jako seznam.
-    """
+    
     maximum = int(maximum)  # Zajistí, že maximum je celé číslo
     prvocisla = [cislo for cislo in range(2, maximum + 1) if je_prvocislo(cislo)]
     return prvocisla
@@ -24,11 +18,7 @@ if __name__ == "__main__":
     prvocisla = vrat_prvocisla(maximum)
     print(prvocisla)
 def je_prvocislo(cislo):
-    """
-    Funkce ověří, zda zadané číslo je nebo není prvočíslo a vrátí True nebo False.
-
-    Prvočíslo je takové číslo větší než 1, které není dělitelné žádným jiným číslem kromě 1 a sebe sama.
-    """
+    
     if cislo < 2:
         return False
     for i in range(2, int(cislo ** 0.5) + 1):
@@ -37,9 +27,7 @@ def je_prvocislo(cislo):
     return True
 
 def vrat_prvocisla(maximum):
-    """
-    Funkce spočítá všechna prvočísla v rozsahu 1 až maximum a vrátí je jako seznam.
-    """
+    
     maximum = int(maximum)  # Zajistí, že maximum je celé číslo
     prvocisla = [cislo for cislo in range(2, maximum + 1) if je_prvocislo(cislo)]
     return prvocisla
